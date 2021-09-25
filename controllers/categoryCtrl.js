bc1be9e57693d8d3dd823ddf4b23f5ff66e61a9a
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-25 13:10:08 +0800

    add/delete category

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -11,11 +11,28 @@ getCategories : async (req,res) =>{
         return res.status(500).json({msg: err.message})
     }
 },
+
 createCategory:async (req,res)=>{
     try {
-        
-        return res.json({msg: "admin detected, welcome to category"})
+        const {name} = req.body;
+        const category = await Category.findOne({name})
+        if(category) return res.status(400).json({msg:"this category already exist"})
+
+        const newCategory = new Category({name})
+
+        await newCategory.save();
+        return res.json({msg: "created category sucessfully"})
        
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+},
+
+deleteCategory: async (req,res)=>{
+    try {
+        
+        await Category.findByIdAndDelete(req.params.id)
+        res.json({msg: "category deleted successfully"})
     } catch (err) {
         return res.status(500).json({msg: err.message})
     }
--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -8,8 +8,8 @@ router.route('/category')
 .get(categoryCtrl.getCategories)
 .post(auth,authAdmin,categoryCtrl.createCategory)
 
-// router.route('/category/:id')
-// .delete(auth,authAdmin,categoryCtrl.deleteCategory)
-// .put(auth,authAdmin,categoryCtrl.updateCategory)
+router.route('/category/:id')
+.delete(auth,authAdmin,categoryCtrl.deleteCategory)
+.put(auth,authAdmin,categoryCtrl.updateCategory)
 
 module.exports = router
