6f34a3c753601755c0f0b600abd7776266181bf2
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-25 15:19:25 +0800

    create/delete product upload

---
+++ controllers/productCtrl.js
@@ -0,0 +1,48 @@
+const Products = require('../models/productModel');
+
+const productCtrl = {
+getProducts: async (req,res) =>{
+    try {
+        
+    } catch (err) {
+        return res.status(500).json({msg : err.message})
+    }
+},
+createProduct: async (req,res) =>{
+    try {
+        const {product_id, title, price, description, content, images, category} = req.body; 
+        if(!images) return res.status(400).json({msg:'No image upload'})
+        
+        const product = await Products.findOne({product_id})
+  
+        if(product) return res.status(400).json({msg: 'This product already exist'})
+  
+        const newProduct = new Products({
+            product_id , title:title.toLowerCase(), price, description, content, images, category
+        })
+      await newProduct.save()
+        return res.json({msg:'Created the Product'})
+      } catch (err) {
+          return res.status(500).json({msg : err.message})
+      }
+},
+deleteProduct: async (req,res) =>{
+    try {
+        await Products.findByIdAndDelete(req.params.id)
+        res.json({msg:'Deleted the Product'})
+    } catch (err) {
+        return res.status(500).json({msg : err.message})
+    }
+},
+updateProduct: async (req,res) =>{
+    try {
+     
+    } catch (err) {
+        return res.status(500).json({msg : err.message})
+    }
+},
+
+
+}
+
+module.exports = productCtrl
