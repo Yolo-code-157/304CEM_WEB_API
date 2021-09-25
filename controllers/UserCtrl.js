d75fd9e7e069eb62a299008d2f4a637ccb120eb7
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-25 08:04:03 +0800

    login/logout auth

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -3,31 +3,30 @@ const bcrypt = require ('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const userCtrl = {
+    
 register:async (req,res)=>{
     try {
-        const {name, email, password} = req.body;
+        const {name, email, password}= req.body;
 
         const user = await Users.findOne({email})
-        if(user) return res.status(400).json({msg: "this email already exist"})
+        if(user) return res.status(400).json({msg:"this email already exists"})
 
-        if(password.length < 6)
-            return res.status(400).json({msg:'password should be at least 6 characters longs'})
+        if(password.length < 6) 
+        return res.status(400).json({msg:'password should be atleast 6 characters longs'})
 
         const passwordHash = await bcrypt.hash(password, 10)
-
         const newUser = new Users({
             name, email, password:passwordHash
         })
         await newUser.save();
         const accesstoken = createAccessToken({id: newUser._id})
         const refreshtoken = createRefreshToken({id: newUser._id})
-
-        res.cookie('refreshtoken', refreshtoken, {
+        res.cookie('refreshtoken' ,refreshtoken,{
             httpOnly:true,
-            path: '/user/refresh_token'
+            path:'/user/refresh_token'
         })
 
-        return res.json({msg: "Registration success"})
+        return res.json({accesstoken})
     } catch (err) {
         return res.status(500).json({msg: err.message})
     }
@@ -36,15 +35,32 @@ register:async (req,res)=>{
 login: async (req,res)=>{
     try {
         const {email, password} = req.body;
-
+    
         const user = await Users.findOne({email})
         if(!user) return res.status(400).json({msg:'user doe not exist'})
-
+    
         const isMatch = await bcrypt.compare(password, user.password)
         if(!isMatch) return res.status(400).json({msg:'incorrect password'})
+    
+    
+        const accesstoken = createAccessToken({id: user._id})
+        const refreshtoken = createRefreshToken({id: user._id})
+            res.cookie('refreshtoken' ,refreshtoken,{
+                httpOnly:true,
+                path:'/user/refresh_token'
+            })
+    
+            return res.json({accesstoken})
+    
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+},
 
-        return res.json({msg: "login success"})
-
+logout: async(req,res)=>{
+    try {
+        res.clearCookie('refreshtoken', {path: '/user/refresh_token' })
+        return res.json({msg:"Logged Out"})
     } catch (err) {
         return res.status(500).json({msg: err.message})
     }
@@ -67,6 +83,17 @@ refreshToken: async (req,res)=>{
         return res.status(500).json({msg: err.message})
     }
     
+},
+
+getUser: async (req,res) =>{
+    try {
+        const user = await Users.findById(req.user.id).select('-password')
+        if(!user) return res.status(400).json({msg:"user does not exist"})
+
+        return res.json(user)
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
 },
 
 } 
