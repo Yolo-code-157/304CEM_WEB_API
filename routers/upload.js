1f309d29e2b6fce1c30ccf5aca1a08c0ac849c98
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-25 14:07:11 +0800

    exception for upload media + remove temp file generate

--- routers/upload.js
+++ routers/upload.js
@@ -1,7 +1,7 @@
 
 const router = require('express').Router();
 const cloudinary = require('cloudinary');
-
+const fs = require ('fs')
 const auth = require('../middlewares/auth');
 const authAdmin = require('../middlewares/authAdmin')
 
@@ -15,11 +15,38 @@ cloudinary.config({
 router.post('/upload', (req,res) =>{
     try {
         console.log(req.files)
-        res.json('checking upload')
+        if(!req.files || Object.keys(req.files).length === 0)
+        return res.status(400).json({msg: 'No files were uploaded'})
+
+        const file= req.files.file;
+
+
+        if(file.size > 1024*1024) {  
+            removeTemp(file.tempFilePath)
+            return res.status(400).json({msg: 'Size is too large'})
+        }
         
+        if( file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+            removeTemp(file.tempFilePath)
+            return res.status(400).json({msg: 'file format is incorrect'})
+        }
+
+        cloudinary.v2.uploader.upload(file.tempFilePath, {folder:"test"},async(err,result)=>{
+            if(err) throw err;
+            removeTemp(file.tempFilePath)
+            res.json({public_id: result.public_id, url: result.secure_url})
+            //res.json({result})
+        })
+
     } catch (err) {
         return res.status(500).json({msg: err.message})
     }
 })
+
+const removeTemp = (path) =>{
+    fs.unlink(path,err =>{
+        if(err) throw err;
+    })
+}
 
 module.exports= router;
