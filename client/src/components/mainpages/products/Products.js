77de9141728310c8d9494419c1e2f23a3ff82da2
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-28 21:11:54 +0800

    Product Item Interface

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from "../../../GlobalState";
+import ProductItem from '../utils/Product_item/ProductItem';
+import "./Product.css"
 
 function Products() {
+    const state = useContext(GlobalState)
+
+    const [products] = state.productsAPI.products
+    
+    console.log(products)
     return (
-        <div>
-            Product List components
+        <div className="products">
+            {
+                products.map(product =>{
+                    return <ProductItem key={product._id} product={product}/>
+                })
+            }
         </div>
     )
 }
