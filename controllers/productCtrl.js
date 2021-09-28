9bc6c6f7c69569f6b970b9df305b2c65db406767
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-28 14:41:04 +0800

    sorting+paginating number of item query

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -21,24 +21,36 @@ class APIfeatures {
         return this;
     }
     sorting(){
+        if(this.queryString.sort){
+            const sortBy= this.queryString.sort.split(',').join(' ')
+           
+            this.query = this.query.sort(sortBy)
 
+        }else{
+            this.query = this.query.sort('createdAt')
+        }
+        return this;
     }
     paginating(){
-
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1 ) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
     }
 }
 
 const productCtrl = {
 getProducts: async (req,res) =>{
     try {
-        const features = new APIfeatures(Products.find(), req.query).filtering()
+        const features = new APIfeatures(Products.find(), req.query).filtering().sorting().paginating()
         const products = await features.query
         
-        // return res.json({
-        //     status: "success",
-        //     result: products.length,
-        //     products
-        // })
+        return res.json({
+            status: "success",
+            result: products.length,
+            products
+        })
         return res.json(products)
     } catch (err) {
         return res.status(500).json({msg : err.message})
