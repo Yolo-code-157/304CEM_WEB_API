b2bff3ad90fcc8be073777a84ad72615920ca2f6
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-09-29 20:20:09 +0800

    local storage login credential in the browser

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,9 +1,23 @@
-import React, {createContext, useState} from 'react'
+import axios from 'axios'
+import React, {createContext, useState, useEffect} from 'react'
 import ProductsAPI from "./api/ProductsAPI"
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) =>{
     const [token, setToken]= useState(false)
+
+    const refreshToken = async () =>{
+        const res = await axios.get('/user/refresh_token')
+        setToken(res.data.accesstoken)
+        // console.log(token)
+    }
+
+    useEffect(() => {
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin) refreshToken()
+        //refreshToken()
+        
+    }, [])
 
     ProductsAPI()
 
