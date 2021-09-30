c9a32b8664c1e1a6c5544a613d02a0f5738a57de
Yolo-code-157 <58157077+Yolo-code-157@users.noreply.github.com>
2021-10-01 02:59:31 +0800

    destroy preview picture

--- client/src/components/mainpages/createProduct/CreateProduct.js
+++ client/src/components/mainpages/createProduct/CreateProduct.js
@@ -8,8 +8,8 @@ const initialState = {
     product_id:'',
     title:'',
     price:0,
-    description:'How to write code . heh he he he he he hee h  e e he e',
-    content:'lorem ipsum is athe dummy text invented to write the nonsense to avoid writing the nonsense like i am writing at that point. Just Avoid it',
+    description:'write description here',
+    content:'Product content here',
     category:'',
     _id: ''
 }
@@ -31,7 +31,7 @@ function CreateProduct() {
     const handleUpload = async (e) =>{
         e.preventDefault()
         try {
-            if(!isAdmin) return alert("You are not an  Admin")
+            if(!isAdmin) return alert("You are not an Admin")
             const file = e.target.files[0]
             if(!file) return   alert("File not exist")
             if(file.size > 1024 * 1024) return alert("Size is too large")
@@ -51,58 +51,94 @@ function CreateProduct() {
         }
     }
 
+    const handleDestroy = async (e) => {
+        try {
+            if(!isAdmin) return alert('You are not an admin');
+            setLoading(true)
+            await axios.post('/api/destroy', {public_id: images.public_id},{
+                headers: {Authorization : token}
+            })
+            setLoading(false)
+            setImages(false)
+    
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+    } 
+
+    const handleChangeInput = e => {
+        const {name, value} = e.target
+        setProduct({...product, [name]:value})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            if(!isAdmin) return alert("you are not Admin")
+            if(!images) return alert("no image found")
+            
+            await axios.post('/api/products', {...product, images}, {
+                headers: {Authorization : token}
+            })
+           
+            setImages(false)
+            setProduct(initialState)
+        } catch (err) {
+            alert(err.response.data.msg)
+        }
+    }
 
     return (
         <div className="create_product">
-            <div className="upload">
-                <input type="file" name="file" id="file-up" onChange={handleUpload}/>
+            <div className="upload"><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <input type="file" name="file" id="file_up" onChange={handleUpload}/>
                 {
-                    loading ? <div id="file_img"><Loading/> </div>
+                    loading ? <div id="file_img"><br/><br/><br/><br/><Loading/> </div>
                 
                 : <div id="file_img" style={styleUpload}>
                     <img src={images ? images.url : ''} alt=""/>
-                    
+                    <span onClick={handleDestroy}>X</span>
                 </div>
                 }
             </div>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div className="row">
                     <label htmlFor="product_id">Product_id</label>
                     <input type="text" name="product_id" id="product_id" 
-                    required value={product.product_id}/>
+                    required value={product.product_id} onChange={handleChangeInput}/>
                 </div>
 
                 <div className="row">
                     <label htmlFor="Title">Title</label>
                     <input type="text" name="title" id="title" 
-                    required value={product.title}/>
+                    required value={product.title} onChange={handleChangeInput}/>
                 </div>
 
                 <div className="row">
                     <label htmlFor="price">Price</label>
                     <input type="text" name="price" id="price" 
-                    required value={product.price}/>
+                    required value={product.price} onChange={handleChangeInput}/>
                 </div>
 
                 <div className="row">
                     <label htmlFor="description">Description</label>
                     <input type="text" name="description" id="description" 
-                    required value={product.description}/>
+                    required value={product.description} onChange={handleChangeInput}/>
                 </div>
 
                 <div className="row">
                     <label htmlFor="content">Content</label>
                     <input type="text" name="content" id="content" 
-                    required value={product.content}/>
+                    required value={product.content} onChange={handleChangeInput}/>
                 </div>
 
                 <div className="row">
-                    <label htmlFor="category">Category</label>
+                    <label htmlFor="category">Category</label><br/>
                     <select name="category" id="category" value={product.category}>
                         <option value="">Please select a category</option>
                         {
                             categories.map(category => (
-                                <option value={category._id}>
+                                <option key={category._id} value={category._id}>
                                     {category.name}
                                 </option>
                                     
@@ -110,6 +146,7 @@ function CreateProduct() {
                         }
                     </select>
                 </div>
+                <br/>
                 <button type="submit">Create Product</button>
             </form>
         </div>
